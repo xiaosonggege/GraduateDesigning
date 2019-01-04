ef277bc5d8517405c02f7c49553424c7d3d03022
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2019-01-04 23:05:06 +0800

    毕设代码有修改

--- Adaboost.py
+++ Adaboost.py
@@ -10,3 +10,26 @@
 @desc:
 '''
 
+import numpy as np
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+x, y = iris.data, iris.target
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state= 32)
+
+clf = DecisionTreeClassifier(max_depth= 2, min_samples_split= 15, min_samples_leaf= 10)
+# clf.fit(x_train, y_train)
+# acc = clf.score(x_test, y_test)
+# print(acc)
+# predict = clf.predict(x_test)
+# print(predict == y_test)
+
+bdt = AdaBoostClassifier(clf, algorithm= 'SAMME', n_estimators= 200, learning_rate= 0.8)
+bdt.fit(x_train, y_train)
+acc = bdt.score(x_test, y_test)
+print(acc)
+
+
