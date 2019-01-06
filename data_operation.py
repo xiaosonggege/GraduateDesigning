3b2e8bd379395fe2c6dbd50f88524057f0c0025f
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2019-01-06 22:58:27 +0800

    毕设代码有修改

--- data_operation.py
+++ data_operation.py
@@ -27,24 +27,33 @@ def SaveFile(data, savepickle_p):
         with open(savepickle_p, 'wb') as file:
             pickle.dump(data, file)
 
-#data为4类数据经过数据均衡、去噪后的矩阵
-data = np.ones(shape= [125050*4, 11]) #如果制作5000个shape= [62550*4, 11], 10000个shape= [125050*4, 11]
-for num in range(3, 7):
-    p = r'D:\GraduateDesigning\ICT DataSet\Label_%s.txt' % num
-    with open(p, 'r') as file:
-        print('正在处理第%d个模型' % num)
-        sub_data = np.loadtxt(file, delimiter=',', skiprows=0)[:200000, :] #如果制作5000时取100000, 10000时取200000
-        i = 0
-        while i < sub_data.shape[0]:
-            if sub_data[i, :].any() == 0 or sub_data[i, -2] < 0:
-                sub_data = np.delete(sub_data, i, axis= 0)
-            else:
-                i += 1
-                print(i)
-        sub_data = np.delete(sub_data, [3, 4, 5], axis= 1)
-        data = sub_data[:125051, :] if data.any() == 0 else np.vstack((data, sub_data[:125051, :]))#如果制作5000时取62551, 10000时取125051
+def EquilibriumDenoising(p_former):
+    '''
+    对数据进行均衡截取、去噪
+    :param p_former: 数据目录前缀
+    :return: 经过均衡化、去噪后的数据集
+    '''
 
-SaveFile(data, savepickle_p= r'D:\GraduateDesigning\data_10000.pickle') #5000/10000
+    # data为4类数据经过数据均衡、去噪后的矩阵
+    data = np.ones(shape=[125050 * 4, 11])  # 如果制作5000个shape= [62550*4, 11], 10000个shape= [125050*4, 11]
+    for num in range(3, 7):
+        # p = r'F:\GraduateDesigning\ICT DataSet\Label_%s.txt' % num
+        p = p_former + r'\\' + r'ICT DataSet\Label_%s.txt' % num
+        with open(p, 'r') as file:
+            print('正在处理第%d个模型' % num)
+            sub_data = np.loadtxt(file, delimiter=',', skiprows=0)[:200000, :]  # 如果制作5000时取100000, 10000时取200000
+            i = 0
+            while i < sub_data.shape[0]:
+                if sub_data[i, :].any() == 0 or sub_data[i, -2] < 0:
+                    sub_data = np.delete(sub_data, i, axis=0)
+                else:
+                    i += 1
+                    print(i)
+            sub_data = np.delete(sub_data, [3, 4, 5], axis=1)
+            data = sub_data[:125051, :] if data.any() == 0 else np.vstack(
+                (data, sub_data[:125051, :]))  # 如果制作5000时取62551, 10000时取125051
+
+    return data
 
 def LoadFile(p):
     '''读取文件'''
@@ -107,6 +116,24 @@ class StatisticStack:
                          self.__interquartile_range, self.__kurtosis, self.__skewness, self.__integral, self.__auto_corr,
                          self.__mean_cross_rate, self.__spectral_energy])
 
+def Acc_h(a, v):
+    '''
+    计算出去除重力加速度和在重力加速度方向上分量后的水平加速度
+    :param a: 加速度计测量的三维加速度坐标
+    :param v: 估算出来的三维重力加速度
+    :return: 水平加速度
+    '''
+
+    #去除重力加速度后的加速度
+    d = a - v
+    #d在重力加速度方向上的分量
+    p = ((d * v) / (v * v)) * v
+    #d在水平方向上的分量
+    h = d - p
+    return h
+
+def
+
 def matrix_operation(data):
     '''
     对进行处理后的数据集进行滑动窗口特征计算，并生成数据矩阵
@@ -128,7 +155,10 @@ def matrix_operation(data):
     #将特征矩阵和标签向量进行组合并返回
     return np.hstack((dataset, data[:, -1][np.newaxis, :]))
 
-
+if __name__ == '__main__':
+    data = EquilibriumDenoising(p_former=r'F:\GraduateDesigning')
+    print(data)
+    # SaveFile(data, savepickle_p= r'F:\GraduateDesigning\data_10000.pickle') #5000/10000
 
 
 
