38d7ce02fa239ac47614730c21b79e8d597de1e9
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2019-01-08 22:55:21 +0800

    毕设代码有修改

---
+++ pointing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: songyunlong
+@license: (C) Copyright 2018-2021, Node Supply Chain Manager Corporation Limited.
+@contact: 1243049371@qq.com
+@software:PyCharm
+@file: pointing
+@time: 2019/1/8 22:46
+@desc:
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
