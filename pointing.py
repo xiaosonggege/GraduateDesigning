19cbbe1b7377e8e900c8c0efae9c1694a95b83ae
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2019-01-08 23:14:41 +0800

    毕设代码有修改

--- pointing.py
+++ pointing.py
@@ -12,3 +12,21 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas
+
+index = np.arange(5)
+bar_width = 0.35
+opacity = 0.4
+means_men = (20, 35, 30, 35, 27)
+figure_1 = plt.figure()
+plt.bar(index, means_men, bar_width, alpha=opacity,
+        color= ['r', 'g', 'b'], label= 'Men')
+plt.xlabel('group')
+plt.ylabel('scores')
+plt.xticks(index , ('A', 'B', 'C', 'D', 'E'))
+plt.legend()
+plt.grid(linestyle= '--')
+plt.ylim(0, 60)
+for a, b in zip(index-0.05, means_men):
+    plt.text(a, b+1, '%.0f' % b)
+plt.show()
