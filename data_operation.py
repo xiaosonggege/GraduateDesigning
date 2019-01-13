f5642b4e6f725c5cda58d80bfe5078a46b76a2eb
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2019-01-13 10:18:39 +0800

    毕设代码有更新

--- data_operation.py
+++ data_operation.py
@@ -144,7 +144,7 @@ def GravityEstimate(origin, series, series_finally):
     :param origin: 初始值(tuple),第一次为(Gest= 9.8, VarIncrease= 0.35, THvar= 1.0)，移动窗过程中每次都使用上一次窗口内最终得到的三值列表
     :param series: 窗口内序列，shape=(100, 3)
     :param series_finally: 扩大窗口长度为原来的4倍后的窗口内数据，shape=(400, 3)
-    :return: 三个方向的重力加速度值,tuple类型， shape= (g, g, g)
+    :return: 更新后的origin元组，其中Gest= (gx, gy, gz)
     '''
     #不变量
     SubNorm = 4
@@ -153,13 +153,15 @@ def GravityEstimate(origin, series, series_finally):
     inc = 0.3
     #origin解析
     Gest, VarIncrease, THvar = origin
-    #计算每帧加速度矢量和
-    acc = np.sum(series**2, axis= 1) #shape= (100,)
-    #计算窗内各个轴上加速度均值和方差
-    W_mean = np.mean(acc)
-    W_var = np.var(acc)
+    #计算窗内加速度合矢量均值和方差
+    W_mean = np.mean(series, axis= 0) #shape=(3,)
+    #计算窗口内平均值的模值
+    W_mean_model = np.sqrt(np.sum(W_mean ** 2))
+    # 计算每帧加速度矢量和
+    acc_model = np.sqrt(np.sum(series**2, axis= 1)) #shape= (100,)
+    W_var = np.var(acc_model)
     #如果滑动窗口内的加速度均值和估计的重力加速度相差较大，则复位方差阈值
-    if np.abs(W_mean - Gest) >= SubNorm:
+    if np.abs(W_mean_model - Gest) >= SubNorm:
         THvar = OriginTHvar
     if W_var < hardThreshold:
         if W_var < THvar:
@@ -169,28 +171,28 @@ def GravityEstimate(origin, series, series_finally):
         else:
             THvar = THvar + VarIncrease
     else:
-        acc_finally = np.sum(series_finally ** 2, axis=1)  # shape= (400,)
-        Gest = np.mean(acc_finally)
+        acc_finally = np.mean(series_finally, axis= 0) #shape= (3,),type= 'ndarray'
+        Gest = acc_finally
     origin_new = [Gest, VarIncrease, THvar]
-    return tuple(origin_new)
+    return tuple(origin_new) #其中Gest=(gx, gy, gz)
 
 
-def Acc_h(a, v):
+def Acc_h(a, g):
     '''
     计算出去除重力加速度和在重力加速度方向上分量后的水平加速度
     :param a: 加速度计测量的三维加速度坐标(ax, ay, az)
-    :param v: 估算出来的三维重力加速度g
-    :return: 水平加速度, 竖直加速度
+    :param g: 估算出来的三维重力加速度g
+    :return: ndarray, (h_magnitude, p)
     '''
-    #v变为3维
-    v = np.array([v, v, v])
+
     #去除重力加速度后的加速度
-    d = a - v #a.shape=(5000/10000, 3), v.shape=(3,)
+    d = a - g #a.shape=(5000/10000, 3), g.shape=(3,)
     #d在重力加速度方向上的分量
-    p = ((d * v) / (v * v)) * v
+    p = np.matmul(d, g[:, np.newaxis]) / np.matmul(g, g[:, np.newaxis]) #shape= (5000, 1)
     #d在水平方向上的分量
-    h = d - p
-    return h, p
+    h = d - p * g[np.newaxis, :] #shape= (5000, 3)
+    h_magnitude = np.sqrt(np.sum(h**2, axis= 1)) #shape= (5000, 1)
+    return np.hstack((h_magnitude, p))
 
 def matrix_operation(data):
     '''
@@ -200,7 +202,7 @@ def matrix_operation(data):
     '''
     dataset = np.zeros(shape= (1, 8*14)) #此处8*14需要修改为8*每列具有的所有特征数（统计+时域+频域）总和
 
-    for i in range(0, data.shape[0], 50):
+    for i in range(0, data.shape[0]-100, 50): #防止越界
         feature_dataset = np.zeros(shape=(1, 14))  # 此处14需要修改为每列具有的所有特征数（统计+时域+频域）总和
         #因为data最后一列为标签
         for j in range(data.shape[-1]-1):
@@ -226,13 +228,15 @@ def data_main(path):
     pri_acc = pri_data[:3, :]
     # 初始化参数元组(在每一次窗口滑动到新位置时会迭代更新)
     origin = tuple([9.8, 0.35, 1.0])
-    for i in range(0, pri_acc.shape[0], 50): #半窗覆盖
+    for i in range(0, pri_acc.shape[0]-100, 50): #防止越界
         #切出shape=(100, 3)矩阵
         series = pri_acc[i:i+100, :]
         #切出4倍窗长度矩阵
         interval_former = (i - 200) if i >= 200 else (i - 0)
         interval_latter = interval_former + 400
         series_finally = pri_acc[interval_former:interval_latter, :]
+        #除了窗口在起始位置外都需要将重力加速度估计值转化为模值
+        origin = tuple([np.sqrt(np.sum(origin[0]**2)), origin[1], origin[-1]]) if i else origin
         origin = GravityEstimate(origin= origin, series= series, series_finally= series_finally)
     #分解加速度为水平、竖直分量
     h, p = Acc_h(pri_acc, origin[0])
